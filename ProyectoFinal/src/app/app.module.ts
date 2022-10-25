2d268c3fdde3f0113214c2fbaed2ca8c27934f8e
MoreNeme <marianonc@hotmail.com>
2022-10-25 13:25:58 -0300

    Tercer Video adentro
    se agregaron los iconos de la redes sociales y el estilo de la pagina, ademas se instalo bootstrap

--- ProyectoFinal/src/app/app.module.ts
+++ ProyectoFinal/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { APlogoComponent } from './components/aplogo/aplogo.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    APlogoComponent
+    APlogoComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
