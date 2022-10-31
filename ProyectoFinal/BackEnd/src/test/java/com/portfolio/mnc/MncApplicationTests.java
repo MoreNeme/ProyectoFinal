1e4a2a3784823deaceabbeb61d8405f7eb025c6a
MoreNeme <marianonc@hotmail.com>
2022-10-31 15:28:15 -0300

    se corrigieron errores y se optimizo el espacio

---
+++ ProyectoFinal/BackEnd/src/test/java/com/portfolio/mnc/MncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mnc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
